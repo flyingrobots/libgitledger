133486f8dfae55ef3f7eba0badfde832614c771b
J. Kirby Ross <james@flyingrobots.dev>
2025-10-23 14:00:38 -0700

    Handled printf results in CLI smoke binaries

--- libgitledger/tests/main.c
+++ libgitledger/tests/main.c
@@ -3,6 +3,8 @@
 
 int main(void) {
     int version = gitledger_version();
-    printf("libgitledger version: %d\n", version);
+    if (printf("libgitledger version: %d\n", version) < 0) {
+        return 1;
+    }
     return 0;
 }
